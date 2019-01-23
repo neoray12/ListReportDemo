ff4f7e6af8dfa167b9349b768b352d0d040f393c
Neo Kung <neo.kung@sap.com>
2019-01-23 08:42:20 +0000

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("listreportdemo.listreportdemo.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("listreportdemo.listreportdemo.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
